9567a13ead0e933324f051bda6df6ea3424fe2c7
iammoses <inawo.moses@gmail.com>
2024-06-08 17:39:10 +0100

    changes added

--- src/pages/Tracking/index.jsx
+++ src/pages/Tracking/index.jsx
@@ -1,35 +1,61 @@
-function Tracking() {
+import { MdOutlineInfo } from "react-icons/md";
 
+function Tracking() {
   return (
     <>
-      <section className="mt-32">
-        <div className="flex justify-center items-center flex-col">
-          <h4 className=" font-semibold text-3xl text-primary uppercase">
+      <section className="m:mt-20">
+        <div className="flex justify-center items-center flex-col ">
+          {/* <h4 className=" font-semibold text-3xl text-primary uppercase">
             Track another shipment
-          </h4>
+          </h4> */}
           <div className="flex items-center gap-1 mt-14">
             <p className=" font-light text-xl">Tracking Number:</p>
-            <p className=" font-semibold text-blue-600 text-xl"> XX123-456-789</p>
+            <p className=" font-semibold text-xl">
+              {" "}
+              XX123-456-789
+            </p>
           </div>
-          <div className="mt-5 text-center">
-            <p className=" uppercase">In transit</p>
+          <div className="flex items-center gap-3 my-5">
+            <span>
+              <MdOutlineInfo color="#4d148c" size={25} />
+            </span>
+            <p className=" text-xl">
+              No scheduled delivery date available at this time
+            </p>
+          </div>
+          <div className="mt-5 text-center  ">
+            <p className="text-5xl font-light">Scheduled delivery: Pending</p>
+          </div>
 
-           <h5 className="text-2xl font-light mt-5" >on FedEx vehicle for delivery <br/>  , Sweden</h5>
-          </div>
+          <img src="/img/tracker.svg" alt="" className="mt-10" />
 
-          <img src="/img/tracker.svg" alt="" className="mt-3" />
-        <div className="lg:w-[50%] w-full flex justify-between items-center mt-20">
-          <div className=" flex flex-col gap-3">
-            <span className=" text-2xl font-semibold text-gray-600">FROM</span>
-            <span className=" text-2xl  text-gray-600 font-light">Sweden</span>
+          <div className="flex flex-col items-center mt-3 gap-5 text-center">
+            <p className=" uppercase font-semibold">In transit</p>
+
+            <h5 className="text-3xl font-light mt-5 text-center">
+              on FedEx vehicle for delivery <br />  Sweden
+            </h5>
+            <p className=" font-semibold text-blue-600 text-2xl uppercase cursor-pointer">
+              {" "}
+              Get status updates
+            </p>
+            <p className="text-2xl">Want to know when your package will arrive?</p>
           </div>
-          <div className=" flex flex-col gap-3">
-            <span className=" text-2xl font-semibold text-gray-600">TO</span>
-            <span className=" text-2xl  text-gray-600 font-light">USA</span>
-          </div>
+          {/* <div className="lg:w-[50%] w-full flex justify-between items-center mt-20">
+            <div className=" flex flex-col gap-3">
+              <span className=" text-2xl font-semibold text-gray-600">
+                FROM
+              </span>
+              <span className=" text-2xl  text-gray-600 font-light">
+                Sweden
+              </span>
+            </div>
+            <div className=" flex flex-col gap-3">
+              <span className=" text-2xl font-semibold text-gray-600">TO</span>
+              <span className=" text-2xl  text-gray-600 font-light">Bear</span>
+            </div>
+          </div> */}
         </div>
-        </div>
-
       </section>
     </>
   );
